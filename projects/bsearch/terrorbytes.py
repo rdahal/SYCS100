2c5105bb8ff4b8e1e3954ba52c50a48f5c5c730c
rdahal <ramhari.dahal@bison.howard.edu>
2013-11-14 16:15:28 -0500

    Team terrorbytes

---
+++ projects/bsearch/terrorbytes.py
@@ -0,0 +1,12 @@
+'Hi professor pratt.
+This is team terrorbytes
+our names:
+    Prajjwal Dangal
+    Ram Hari Dahal
+    Nischal Baral
+    Roshan Thapaliya
+    Gyasi Jordan
+    Tyriece McGlawn
+    Jonathon Moody'
+    
+    
